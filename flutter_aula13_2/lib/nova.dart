83712a0b7ac946d462ffb153f410fd16c39b9c2a
maurojh <maurojh@gmail.com>
2024-07-01 21:22:50 -0300

    Update nova.dart

--- flutter_aula13_2/lib/nova.dart
+++ flutter_aula13_2/lib/nova.dart
@@ -16,17 +16,16 @@ Future<String> carregaDados() async {
   return Dados.original;
 }
 
-
 class _NovaState extends State<Nova> {
-  late DadosJSON meusDados;
+  late Future<DadosJSON> meusDados;
 
   @override
-  void initState() async {
-    meusDados = await buscaDados();
+  void initState() {
     super.initState();
+    meusDados = buscaDados();
   }
 
-  Future buscaDados() async {
+  Future<DadosJSON> buscaDados() async {
     String original = await carregaDados();
     final decodificada = json.decode(original);
     return DadosJSON.fromJson(decodificada);
@@ -41,7 +40,19 @@ class _NovaState extends State<Nova> {
         ),
         body: Center(
           // nomes virão de outro lugar
-          child: Text(meusDados.s2),
+          child: FutureBuilder<DadosJSON>(
+            future: meusDados,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator(); // Exibir um indicador de carregamento enquanto espera os dados
+              } else if (snapshot.hasError) {
+                return Text('Erro ao carregar dados: ${snapshot.error}');
+              } else {
+                return Text(snapshot
+                    .data!.s2); // Exibir os dados quando estiverem prontos
+              }
+            },
+          ),
         ),
       ),
     );
